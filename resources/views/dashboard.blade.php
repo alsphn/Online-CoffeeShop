fcc6dad43cfecbef810e16557c2ffa6a79b67056
Al Li <alya.liaqmi@gmail.com>
2025-11-11 15:08:31 +0700

    update adminlte

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.admin') {{-- Memanggil layout master Anda --}}
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Halaman Dasbor Saya</h3>
+                </div>
+                <div class="card-body">
+                    Selamat datang di halaman AdminLTE yang terintegrasi!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
